ef2241bd50a587e26b99d6859624227230ed853f
nostalgiatune <nostalgiatune@gmail.com>
2016-04-29 15:01:32 +0300

    Solution

--- src/main/java/com/tuoppi/multipleconfigurations/config/RootContextWebSecurityConfig.java
+++ src/main/java/com/tuoppi/multipleconfigurations/config/RootContextWebSecurityConfig.java
@@ -7,6 +7,7 @@ import com.tuoppi.multipleconfigurations.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,6 +22,12 @@ public class RootContextWebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDao dao;
     
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+    
     @Bean @Transactional
     public UserDetailsService userDetailsService() {
         return new MyUserDetailsService(dao);
@@ -28,14 +35,14 @@ public class RootContextWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        //super.configure(auth);
         auth.userDetailsService(userDetailsService());
         System.out.println("aob ok");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        //super.configure(http);
         http.formLogin().and().authorizeRequests().antMatchers("/**").denyAll();
     }
     
